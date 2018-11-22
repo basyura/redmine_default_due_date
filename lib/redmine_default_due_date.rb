e5140beb12591eeab47d998b6ce3142db7b78b9d
Roberto Piccini <roberto.piccini@gmail.com>
2018-11-22 13:04:52 +0100

    refactoring: make available to public interface the number of days after

--- lib/redmine_default_due_date.rb
+++ lib/redmine_default_due_date.rb
@@ -2,8 +2,11 @@
 module Redmine
   module Plugins
     class DefaultDueDateListener < Redmine::Hook::ViewListener
+
+      NUMBER_DAYS_AFTER = 7
+
       def view_issues_new_top(context)
-        context[:issue].due_date ||= Date.today + 7
+        context[:issue].due_date ||= Date.today + NUMBER_DAYS_AFTER.days
       end
     end
   end
